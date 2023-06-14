2b4014328f39873956c8b7670aa9ecd38cbffb8c
AliYalcin <aliyalcin0001@gmail.com>
2023-06-15 00:33:51 +0300

    Added firebase configurations and tested still ongoing authentication process. Also little added mvc structure

---
+++ lib/utils/route_generation.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:guide_up/main.dart';
+import 'package:guide_up/pages/login/login_page.dart';
+
+import '../pages/other/error_page.dart';
+
+class RouteGenerator {
+  static Route<dynamic>? _createRoute(
+      Widget routeWidget, RouteSettings settings) {
+    if (defaultTargetPlatform == TargetPlatform.iOS) {
+      return CupertinoPageRoute(
+        settings: settings,
+        builder: (builder) => routeWidget,
+      );
+    } else if (defaultTargetPlatform == TargetPlatform.android) {
+      return MaterialPageRoute(
+        settings: settings,
+        builder: (builder) => routeWidget,
+      );
+    } else {
+      return CupertinoPageRoute(
+        settings: settings,
+        builder: (builder) => routeWidget,
+      );
+    }
+  }
+
+  static Route<dynamic>? routeGeneration(RouteSettings settings) {
+    switch (settings.name) {
+      case '/main':
+        return _createRoute(const MyApp(), settings);
+      case '/':
+        return _createRoute(const LoginPage(), settings);
+      default:
+        return _createRoute(const ErrorPage(), settings);
+    }
+  }
+}
