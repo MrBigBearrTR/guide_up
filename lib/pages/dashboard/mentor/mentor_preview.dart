f15f728ca6d0287f328242b3bde6dab5251a4c28
ecesariyerr <ecesariyerr@gmail.com>
2023-07-10 11:10:06 +0300

    mentor preview page oluşturuldu

---
+++ lib/pages/dashboard/mentor/mentor_preview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MentorPreview extends StatefulWidget {
+  const MentorPreview({Key? key}) : super(key: key);
+
+  @override
+  State<MentorPreview> createState() => _MentorPreviewState();
+}
+
+class _MentorPreviewState extends State<MentorPreview> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
