918f66848f15682a2e871d07fd00ef3e9d5a4e5e
ecesariyerr <ecesariyerr@gmail.com>
2023-07-12 11:11:12 +0300

    mentor sayfasındaki yorumlar sayfası oluşturuldu

---
+++ lib/pages/dashboard/mentor/mentor_comments.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MentorComments extends StatefulWidget {
+  const MentorComments({Key? key}) : super(key: key);
+
+  @override
+  State<MentorComments> createState() => _MentorCommentsState();
+}
+
+class _MentorCommentsState extends State<MentorComments> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
